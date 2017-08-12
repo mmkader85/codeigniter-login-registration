a8e2fd0e70d85ba29c5dcb28679ede5764e096cc
Mohamed <mohamed.ak@jwt.com>
2017-08-12 15:59:43 +0800

    htacess added. required helpers loaded.

--- application/controllers/Start.php
+++ application/controllers/Start.php
@@ -3,23 +3,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Start extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('start');
-	}
+    /*public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper(array('url', 'html'));
+        $this->load->library('session');
+    }*/
+
+    public function index()
+    {
+        $this->load->view('start');
+    }
+
+    public function logout()
+    {
+        $data = array('login' => '', 'uname' => '', 'uid' => '');
+        $this->session->unset_userdata($data);
+        $this->session->sess_destroy();
+        redirect('start/index');
+    }
 }
